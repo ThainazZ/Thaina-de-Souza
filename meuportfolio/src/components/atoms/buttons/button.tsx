552fd36c02579621ac11cfcc556472268bef6d43
ThainazZ <thaina.dias@grendene.com.br>
2023-07-07 13:44:32 -0300

    primeiro commit

---
+++ meuportfolio/src/components/atoms/buttons/button.tsx
@@ -0,0 +1,14 @@
+import * as S from "./styles";
+
+
+//botão toggle-switch de tradução
+
+const Button = () => {
+
+    return <S.Container>PT-BR</S.Container>;
+
+
+}
+
+
+export default Button;
---
+++ meuportfolio/src/styles/global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+//estilo global da tela
+
+const globalStyle = createGlobalStyle `
+
+
+
+
+`
+
+export default globalStyle;
