fa41a726cbffe6a4662ffc07cc29d61c2ae7b2ce
ThainazZ <thaina.dias@grendene.com.br>
2023-07-18 12:51:30 -0300

    styling the right carousel

--- meuportfolio/src/components/molecules/RightCard/index.tsx
+++ meuportfolio/src/components/molecules/RightCard/index.tsx
@@ -2,42 +2,35 @@ import { useTranslation } from 'react-i18next';
 import * as S from './styles';
 import reactImage from '../../../assets/React.png';
 import typeImage from '../../../assets/Typescript.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 const RightCard = () => {
-    const { t, i18n } = useTranslation();
-    return (
-        <S.Container>
-             <p
-             style={{
-                fontSize: '30px',
-                fontWeight: '500',
-                paddingTop: '5%',
-             }}
-             >
-                {t('main.title')}
-             </p>
-             <div className='Box'
-             style={{width:'200px', height:'250px', display: 'flex', alignItems: 'center', overflow:'hidden', overflowX:'scroll', scrollbarWidth: 'none', }}>
+  const { t, i18n } = useTranslation();
 
-               <div className='React' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <img src={reactImage} style={{width: '180px', paddingTop: '5%',}}></img>
-                <p style={{width: '200px', textAlign: 'center', paddingTop: '3%',}}>
-                Trabalho diariamente com React e aplicações webs;</p>
-                </div> 
-
-                <div className='Type' style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                <img src={typeImage} style={{width: '180px', paddingTop: '5%',}}></img>
-                <p style={{width: '200px', textAlign: 'center', paddingTop: '3%',}}>
-                Utilizo em grande parte o Typesc;</p>
-                </div> 
-
-
-             </div>
-           
-        </S.Container>
-        
-        
-    );
+  return (
+    <S.Container>
+      <p style={{fontSize: '40px', paddingBottom:'2%'}}>
+        {t('main.title')}
+      </p>
+      <div className='Box' style={{maxWidth: '300px', maxHeight: '350px'}}>
+        <Carousel infiniteLoop useKeyboardArrows swipeable showThumbs={false} emulateTouch showStatus={false} autoPlay>
+          <div className='React' style={{padding:'4%'}}>
+            <img src={reactImage} style={{width: '150px', height: '150px'}} alt='React' />
+            <p style={{padding: '8%'}}>
+              Trabalho diariamente com React e aplicações webs;
+            </p>
+          </div>
+          <div className='Type' style={{padding:'4%'}}>
+            <img src={typeImage} style={{width: '150px', height: '150px'}} alt='TypeScript' />
+            <p style={{padding: '8%'}}>
+              Utilizo em grande parte o Typescript;
+            </p>
+          </div>
+        </Carousel>
+      </div>
+    </S.Container>
+  );
 };
 
 export default RightCard;
--- meuportfolio/src/components/molecules/RightCard/styles.ts
+++ meuportfolio/src/components/molecules/RightCard/styles.ts
@@ -15,7 +15,6 @@ border-radius: 10px;
 background: var(--secondary-linear);
 backdrop-filter: blur(30px);
 box-shadow: 0px 2px 30px 0px rgba(49, 10, 49, 0.50);
-::-webkit-scrollbar {display:none;}
 
 
 
