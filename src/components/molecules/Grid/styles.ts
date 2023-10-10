929d45464db94bd1d5f2a5baf5ae2e520d6577c1
Thaina de Souza Dias <thaina.dias@grendene.com.br>
2023-10-10 07:58:10 -0300

    fix: repositorio do git

---
+++ src/components/molecules/Grid/styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components";
+import "../../../styles/variables.css";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  column-gap: 1rem;
+  row-gap: 1rem;
+`;
+
+export const Box = styled.div`
+  width: 350px;
+  height: 350px;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  align-items: center;
+  font-weight: 300;
+  border-radius: 10px;
+  background: var(--secondary-linear);
+  backdrop-filter: blur(30px);
+  box-shadow: 0px 2px 30px 0px rgba(49, 10, 49, 0.5);
+
+  @media (max-width: 640px) {
+    width: 300px;
+  }
+
+  @media (min-width: 641px) and (max-width: 968px) {
+    width: 300px;
+  }
+`;
+
+export const Card = styled.div`
+  margin: 2rem 2rem 0rem 2rem;
+`;
+
+export const Button = styled.button`
+  margin: 1rem 1rem 1.5rem 1rem;
+  cursor: pointer;
+  color: white;
+  border: none;
+  text-decoration: none;
+  outline: none;
+  height: 3rem;
+  width: 7rem;
+  background: var(--secondary-linear);
+  backdrop-filter: blur(30px);
+  box-shadow: 0px 2px 30px 0px rgba(49, 10, 49, 0.5);
+
+  &:hover {
+    background-color: white;
+    color: purple;
+  }
+`;
