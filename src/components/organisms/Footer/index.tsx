84c4d7710b1686bfe388036220f761daf23a37f4
Thaina de Souza Dias <thaina.dias@grendene.com.br>
2023-10-12 08:38:09 -0300

    fix: espaçamento e add: experience

--- src/components/organisms/Footer/index.tsx
+++ src/components/organisms/Footer/index.tsx
@@ -1,74 +1,81 @@
-import * as S from './styles';
-import githubImage from '../../../assets/github.png';
-import linkedinImage from '../../../assets/linkedin.png';
-import { useTranslation } from 'react-i18next';
-import styled from 'styled-components';
+import * as S from "./styles";
+import githubImage from "../../../assets/github.png";
+import linkedinImage from "../../../assets/linkedin.png";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
 
 function Final() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-
-    const LinkedinLink = styled.a `
+  const LinkedinLink = styled.a`
     display: inline-block;
     transition: transform 0.3s ease;
     &:hover {
-        transform: scale(1.1); // Increase the scale to make the image bigger on hover
-      }
-    `;
+      transform: scale(
+        1.1
+      ); // Increase the scale to make the image bigger on hover
+    }
+  `;
 
-    const LinkedinImage = styled.img`
+  const LinkedinImage = styled.img`
     width: 70px;
     padding-bottom: 8%;
     margin-right: 20px;
-    `;
+  `;
 
-
-
-    const GithubLink = styled.a `
+  const GithubLink = styled.a`
     display: inline-block;
     transition: transform 0.3s ease;
     &:hover {
-        transform: scale(1.1); // Increase the scale to make the image bigger on hover
-      }
-    `;
+      transform: scale(
+        1.1
+      ); // Increase the scale to make the image bigger on hover
+    }
+  `;
 
-    const GithubImage = styled.img`
+  const GithubImage = styled.img`
     width: 70px;
     padding-bottom: 8%;
     margin-right: 20px;
-    `;
+  `;
 
-    return (
-        <S.Container>
-            <div>
-                <p style={{color:'white', fontSize: '50px',}}>{t('footer.title')}</p>
-            </div>
-            <br></br>
-            <div>
-                <p style={{color:'white', fontSize:'30px', fontWeight:'300'}}>{t('footer.text')}</p>
-            </div>
-            <br></br>
-          
-            
-            <div style={{paddingTop: '2rem', alignItems:'center'}}>
-                <LinkedinLink href='https://www.linkedin.com/in/thain%C3%A1-souza-799a85186/' target='_blank'>
-                <LinkedinImage src={linkedinImage} style={{
-                width:'70px', 
-                paddingBottom: '8%', 
-                margin:'0 20px 70px 0px', 
-                }}></LinkedinImage></LinkedinLink>
-                
-        <GithubLink href='https://github.com/ThainazZ' target='_blank'>
-            <GithubImage src={githubImage} style={{
-                width:'70px', 
-                paddingBottom: '8%', 
-                margin:'0 0px 70px 20px',
-               
-                }}></GithubImage></GithubLink>
-                </div>
-        </S.Container>
-        
-    );
-};
+  return (
+    <S.Container>
+      <div>
+        <p style={{ color: "white", fontSize: "30px", fontWeight: "300" }}>
+          {t("footer.text")}
+        </p>
+      </div>
+      <br></br>
+
+      <div style={{ alignItems: "center" }}>
+        <LinkedinLink
+          href="https://www.linkedin.com/in/thain%C3%A1-souza-799a85186/"
+          target="_blank"
+        >
+          <LinkedinImage
+            src={linkedinImage}
+            style={{
+              width: "70px",
+              paddingBottom: "8%",
+              margin: "0 20px 70px 0px",
+            }}
+          ></LinkedinImage>
+        </LinkedinLink>
+
+        <GithubLink href="https://github.com/ThainazZ" target="_blank">
+          <GithubImage
+            src={githubImage}
+            style={{
+              width: "70px",
+              paddingBottom: "8%",
+              margin: "0 0px 70px 20px",
+            }}
+          ></GithubImage>
+        </GithubLink>
+      </div>
+    </S.Container>
+  );
+}
 
 export default Final;
--- src/components/organisms/Footer/styles.ts
+++ src/components/organisms/Footer/styles.ts
@@ -7,11 +7,11 @@ export const Container = styled.div `
 display: flex;
 flex-direction: column;
 text-align: center;
-padding-top: 10%;
+padding-top: 3%;
 
 
 @media  (max-width: 640px) {
-    padding-top: 10%;
+    padding-top: 3%;
     padding-left: 20%;
     padding-right: 20%;
 
